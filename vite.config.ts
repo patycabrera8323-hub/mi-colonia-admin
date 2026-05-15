079ac255daa12e8d6c31a40a8612cc375dabb88a
yi890 <jicr109@gmail.com>
2026-05-14 19:24:27 -0600

    Standardize manifest filename to manifest.json

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig(({mode}) => {
       tailwindcss(),
       VitePWA({
         registerType: 'autoUpdate',
+        manifestFilename: 'manifest.json',
         workbox: {
           skipWaiting: true,
           clientsClaim: true,
